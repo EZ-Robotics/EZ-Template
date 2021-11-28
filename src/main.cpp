c0cc5ef8e547ab82d5fe00a14d8148462a24db59
Jess Zarchi <jackfz01@gmail.com>
2021-11-28 13:09:54 -0700

    2.0.0-RC4

--- src/main.cpp
+++ src/main.cpp
@@ -5,24 +5,29 @@
 // Chassis constructor
 Drive chassis (
   // Left Chassis Ports (negative port will reverse it!)
-  {-11, -5, -7}
+  //   the first port is the sensored port (when trackers are not used!)
+  {1, 2}
 
   // Right Chassis Ports (negative port will reverse it!)
-  ,{3, 2, 17}
+  //   the first port is the sensored port (when trackers are not used!)
+  ,{-3, -4}
 
   // IMU Port
-  ,18
+  ,5
 
   // Wheel Diameter (Remember, 4" wheels are actually 4.125!)
   //    (or tracking wheel diameter)
-  ,3.25
+  ,4.125
 
   // Cartridge RPM
   //   (or tick per rotation if using tracking wheels)
   ,600
 
-  // External Gear Ratio 
+
+  // External Gear Ratio (MUST BE DECIMAL)
   //    (or gear ratio of tracking wheel)
+  // eg. if your drive is 84:36 where the 36t is powered, your RATIO would be 2.333.
+  // eg. if your drive is 36:60 where the 60t is powered, your RATIO would be 0.6.
   ,1.66666666667
 
   // Uncomment if using tracking wheels
@@ -38,7 +43,6 @@ Drive chassis (
   // 3 Wire Port Expander Smart Port
   // ,1
 );
-
 
 
 /**
