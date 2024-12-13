32b8790e2379b924cac38e9a50d4167f41b89c12
Jess Zarchi <jackfz01@gmail.com>
2024-12-12 22:08:06 -0800

    Current version 3.2.0

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -48,7 +48,7 @@ const config = {
           lastVersion: 'current',
           versions: {
             current: {
-              label: '3.1.0',
+              label: '3.2.0',
               banner: 'none',
             },
             '2.x': {
